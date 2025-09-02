016bdfbb4884d36fa602accd545e21d47fd6aeba
cwd <cwdbj888@163.com>
2025-09-02 11:11:53 +0800

    nft 开发完成

--- test/MoodNftTest.sol
+++ test/MoodNftTest.sol
@@ -125,3 +125,8 @@ contract MoodNftTest is Test{
         return "Mood NFT";
     }
 }   
+
+/**
+ * cast send 0x5FbDB2315678afecb367f032d93F642f64180aa3 "flipMood(uint256)" 0  --rpc-url http://localhost:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 
+ * cast send 0x5FbDB2315678afecb367f032d93F642f64180aa3 "mintNFT()" --rpc-url http://localhost:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80  
+*/
